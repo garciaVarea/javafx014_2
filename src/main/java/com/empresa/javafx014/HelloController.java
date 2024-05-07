044a219afef2d6c035173a0c211e922c6c77dd14
garciaVarea <jesusangel.garciavarea.23@campusfp.es>
2024-05-07 14:34:50 +0200

    mensaje

--- src/main/java/com/empresa/javafx014/HelloController.java
+++ src/main/java/com/empresa/javafx014/HelloController.java
@@ -1,14 +1,48 @@
 package com.empresa.javafx014;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtContraseña;
+
+    @FXML
+    private TextField txtContraseña2;
+
+    @FXML
+    private CheckBox cbPrivacidad;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        if (!txtCorreo.getText().isEmpty() && !txtContraseña.getText().isEmpty() && !txtContraseña2.getText().isEmpty() && cbPrivacidad.isSelected()) {
+            welcomeText.setText("Usuario registrado");
+        } else {
+            welcomeText.setText("Faltan datos");
+        }
+    }
+
+    @FXML
+    protected void onLoginButtonClick() throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 200);
+        stage.setTitle("Iniciar sesión");
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/com/empresa/javafx014/LoginController.java
+++ src/main/java/com/empresa/javafx014/LoginController.java
@@ -1,2 +1,29 @@
-package com.empresa.javafx014;public class LoginController {
+package com.empresa.javafx014;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    protected void onLoginButtonClick() {
+        String email = txtEmail.getText();
+        String password = txtPassword.getText();
+
+        if ("admin@gmail.com".equals(email) && "123".equals(password)) {
+            lblStatus.setText("Inicio correcto");
+        } else {
+            lblStatus.setText("Correo o contraseña incorrectos");
+        }
+    }
 }
